18478aad497f1c3abfff41e423f60bcd665d81b8
gmc314 <grant.cheng1@gmail.com>
2024-01-30 18:02:41 -0500

    add pieces classes

--- back-end.py
+++ back-end.py
@@ -10,9 +10,65 @@ BOARD = [
 ]
 
 class Piece:
-    def __init__(self, color: str, name: str, id, location: tuple, isThreatened: bool) -> None:
+    def __init__(self, color: str, name: str, id: str, location: tuple, canCastle: bool, points: int) -> None:
         self.id = id
         self.color = color 
         self.name = name
         self.location = location
-        self.isThreatened = isThreatened
+        self.canCastle = False
+        self.points = points
+
+# inheriting from Piece class
+class King(Piece):
+    def __init__(self, color: str, id, location: tuple) -> None:
+        self.id = id
+        self.color = color 
+        self.name = "King"
+        self.location = location
+        self.canCastle = True
+        self.points = 0
+
+class Queen(Piece):
+    def __init__(self, color: str, id, location: tuple) -> None:
+        self.id = id
+        self.color = color 
+        self.name = "Queen"
+        self.location = location
+        self.points = 9
+
+class Rook(Piece):
+    def __init__(self, color: str, id, location: tuple) -> None:
+        self.id = id
+        self.color = color 
+        self.name = "Rook"
+        self.location = location
+        self.canCastle = True
+        self.points = 5
+
+class Bishop(Piece):
+    def __init__(self, color: str, id, location: tuple) -> None:
+        self.id = id
+        self.color = color 
+        self.name = "Bishop"
+        self.location = location
+        self.canCastle = True
+        self.points = 3
+
+class Knight(Piece):
+    def __init__(self, color: str, id, location: tuple) -> None:
+        self.id = id
+        self.color = color 
+        self.name = "Knight"
+        self.location = location
+        self.canCastle = True
+        self.points = 3
+
+class Pawn(Piece):
+    def __init__(self, color: str, id, location: tuple) -> None:
+        self.id = id
+        self.color = color 
+        self.name = "Pawn"
+        self.location = location
+        self.canCastle = True
+        self.points = 1
+
