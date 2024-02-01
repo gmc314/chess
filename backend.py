4d085fa25d0a427c6031e4ebcda96ee8a4cc123c
gmc314 <grant.cheng1@gmail.com>
2024-01-31 20:39:32 -0500

    bebug bishop inheritance

--- backend.py
+++ backend.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 BOARD = [
     [None, None, None, None, None, None, None, None],
     [None, None, None, None, None, None, None, None],
@@ -17,8 +15,8 @@ fileIndex = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
 # gets the indices of the Board from rank and file 
 def getBoardIndexFromRankAndFile(square: tuple):
     file, rank = square
-    col = int(rank) - 1
-    row = fileIndex[file]
+    col = fileIndex[file]
+    row = int(rank) - 1
     return (row, col)
 
 
@@ -35,17 +33,18 @@ def stringifyRankFile(square: tuple):
     return f"{square[0]}{square[1]}"
 
 
+
 class Piece:
-    def __init__(self, color: str, name: str, id: str, location: tuple, canCastle: bool, points: int) -> None:
-        self.id = id
+    def __init__(self, color: str, name: str, ID: str, location: tuple, canCastle: bool, points: int):
+        self.ID = ID
         self.color = color 
         self.name = name
         self.location = location
-        self.canCastle = False
+        self.canCastle = canCastle
         self.points = points
 
     def __repr__(self) -> str:
-        return f"{self.color} {self.name} at {stringifyRankFile(self.location)}"
+        return f"{self.color} {self.name}"
 
     # move piece from current square to new `square`
     @classmethod
@@ -80,7 +79,7 @@ class Piece:
     
     @classmethod
     def isMoveValid(self, square):
-        pass
+        return True
 
     @classmethod
     def getAllValidMoves(self):
@@ -96,56 +95,49 @@ class Piece:
 
 # inheriting from Piece class
 class King(Piece):
-    def __init__(self, color: str, id, location: tuple) -> None:
-        self.id = id
-        self.color = color 
-        self.name = "King"
-        self.location = location
+    def __init__(self, color, name,ID, location, canCastle, points):
+        super().__init__(self, color, name, ID, location, canCastle, points)
+        self.name = "King" 
         self.canCastle = True
         self.points = 0
 
 class Queen(Piece):
-    def __init__(self, color: str, id, location: tuple) -> None:
-        self.id = id
-        self.color = color 
+    def __init__(self, color, name, ID, location, canCastle, points):
+        super().__init__(self, color, name, ID, location, canCastle, points) 
         self.name = "Queen"
-        self.location = location
         self.points = 9
 
 class Rook(Piece):
-    def __init__(self, color: str, id, location: tuple) -> None:
-        self.id = id
-        self.color = color 
+    def __init__(self, color, name, ID, location, canCastle, points):
+        super().__init__(color, name, ID, location, canCastle, points) 
         self.name = "Rook"
-        self.location = location
         self.canCastle = True
         self.points = 5
 
 class Bishop(Piece):
-    def __init__(self, color: str, id, location: tuple) -> None:
-        self.id = id
-        self.color = color 
-        self.name = "Bishop"
+    def __init__(self, color, ID, location):
+        super().__init__(color, "Bishop", ID, location, False, 3) 
+        self.ID = ID
         self.location = location
-        self.canCastle = True
-        self.points = 3
+        self.color = color
+        
+
 
 class Knight(Piece):
-    def __init__(self, color: str, id, location: tuple) -> None:
-        self.id = id
-        self.color = color 
+    def __init__(self, color, name, ID, location, canCastle, points):
+        super().__init__(self, color, name, ID, location, canCastle, points) 
         self.name = "Knight"
-        self.location = location
-        self.canCastle = True
         self.points = 3
 
 class Pawn(Piece):
-    def __init__(self, color: str, id, location: tuple) -> None:
-        self.id = id
-        self.color = color 
+    def __init__(self, color, name, ID, location, canCastle, points):
+        super().__init__(self, color, name, ID, location, canCastle, points) 
         self.name = "Pawn"
-        self.location = location
-        self.canCastle = True
         self.points = 1
 
 
+def placePiece(piece: Piece):
+    currentSquare = piece.location
+    row, col = getBoardIndexFromRankAndFile(currentSquare)
+    BOARD[row][col] = piece
+    return f"{piece} placed"
