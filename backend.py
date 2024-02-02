68db6dc51d1978c6913f2488b438fb8ca5388d30
gmc314 <grant.cheng1@gmail.com>
2024-02-01 19:36:16 -0500

    debug getRankFile

--- backend.py
+++ backend.py
@@ -15,18 +15,17 @@ fileIndex = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
 
 
 # gets the indices of the Board from rank and file 
-def getBoardIndexFromRankAndFile(square: tuple):
+def getBoardIndexFromRankAndFile(square: tuple[str, int]):
     file, rank = square
     col = fileIndex[file]
-    row = len(BOARD) - int(rank)
+    row = len(BOARD) - rank
     return (row, col)
 
 
 # gets the rank and file from indices of the Board
-def getRankAndFileFromBoardIndex(indices: tuple):
-    row, col = indices
+def getRankAndFileFromBoardIndex(row, col):
     file = list(fileIndex.keys())[list(fileIndex.values()).index(col)]
-    rank = row + 1
+    rank = len(BOARD) - row 
     return (file, rank)
 
 
@@ -37,14 +36,17 @@ def stringifyRankFile(square: tuple):
 
 class Piece:
     def __init__(self, color: str, name: str, ID: str, location: tuple, canCastle: bool, points: int):
-        self.ID = ID
         self.color = color 
         self.name = name
+        self.ID = ID
         self.location = location
         self.canCastle = canCastle
         self.points = points
 
     def __repr__(self) -> str:
+        return f"{self.color} {self.name}"
+
+    def __str__(self) -> str:
         return f"{self.color} {self.name}"
 
 
@@ -99,31 +101,33 @@ class Knight(Piece):
         return True    
 
 class Pawn(Piece):
-    def __init__(self, color, ID, location, firstTurn):
+    def __init__(self, color, ID, location):
         super().__init__(color, "Pawn", ID, location, False, 1) 
-        self.firstTurn = firstTurn
-    
-    @classmethod
+        self.firstTurn = True
+
     def isMoveValid(self, newSquare):
+        # getting files and ranks of currrent and new squares
         currentFile, currentRank = self.location
         newFile, newRank = newSquare
 
         if self.color == "White":
             if self.firstTurn == True:
+                # returns a boolean 
                 return (currentFile == newFile) and (1 <= newRank - currentRank <= 2)
         
             else:
+                # returns a boolean 
                 return (currentFile == newFile) and (newRank - currentRank == 1)
         
         if self.color == "Black":
             if self.firstTurn == True:
+                # returns a boolean 
                 return (currentFile == newFile) and (1 <= currentRank - newRank <= 2)
         
             else:
+                # returns a boolean 
                 return (currentFile == newFile) and (currentRank - newRank == 1)
         
-
-
 
 # place a piece on the board at its initial square
 def placePiece(piece: Piece):
@@ -138,25 +142,28 @@ def moveFromCurrentSquare(piece: Union[King, Queen, Rook, Bishop, Knight, Pawn],
     if not piece.isMoveValid(newSquare):
         return "invalid move"
     
-    # condition for en passant and first turn two-square forward move
-    if type(piece) == Pawn:
-        piece.firstTurn = False
-
     # getting current location of piece
     currentSquare = piece.location
     currentRow, currentCol = getBoardIndexFromRankAndFile(currentSquare)
 
     # getting location of move
     newRow, newCol = getBoardIndexFromRankAndFile(newSquare)
-    newRank, newFile = getRankAndFileFromBoardIndex((newRow, newCol))
 
+    newRank, newFile = getRankAndFileFromBoardIndex(newRow, newCol)
+    print(newRank, newFile)
     # move piece from the current square
     BOARD[currentRow][currentCol] = None
 
     # to new
-    piece.location = (newFile, newRank)
+    piece.location = (newRank, newFile)
+    print(piece.location)
 
     BOARD[newRow][newCol] = piece
+
+    # condition for en passant and first turn two-square forward move
+    if type(piece) == Pawn:
+        piece.firstTurn = False
+
     # print out the move 
     return f"{piece.name} {stringifyRankFile(currentSquare)} to {stringifyRankFile(newSquare)}"
 
