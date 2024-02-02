3b62f08f856bd381852561b638820dd9ef54a6ba
gmc314 <grant.cheng1@gmail.com>
2024-02-01 20:04:28 -0500

    test invalid pawn move

--- test.py
+++ test.py
@@ -2,12 +2,31 @@ from backend import *
 from pprint import pprint
 
 # testing placing a piece on the board
-bb = Pawn("White", "1", ("b", 2))
-placePiece(bb)
-print(type(BOARD[6][1])==Pawn)
+wb = Bishop("White", "1", ("b", 2))
+placePiece(wb)
+print(type(BOARD[6][1])==Bishop)
 
 # testing moving piece
-print(moveFromCurrentSquare(bb, ("b", 4)))
+moveFromCurrentSquare(wb, ("a", 1))
 print(BOARD[6][1]==None)
-print(type(BOARD[4][1])==Pawn)
+print(type(BOARD[7][0])==Bishop)
 
+########
+# PAWN 
+# testing placing a pawn on the board
+wp = Pawn("White", "1", ("d", 2))
+placePiece(wp)
+print(type(BOARD[6][3])==Pawn)
+print(wp.firstTurn)
+
+# testing moving piece
+moveFromCurrentSquare(wp, ("d", 4))
+print(BOARD[6][3]==None)
+print(type(BOARD[4][3])==Pawn)
+print(not wp.firstTurn)
+
+# testing invalid move
+bp = Pawn("Black", "1", ("h", 7))
+placePiece(bp)
+print(type(BOARD[1][7])==Pawn)
+print(moveFromCurrentSquare(bp, ("h", 4)) == "invalid move")
