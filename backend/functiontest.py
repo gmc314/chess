141ffb6bf4693c9d2b0e963cb7fed271ffc94095
gmc314 <grant.cheng1@gmail.com>
2024-02-22 18:19:40 -0500

    restore unit tests

--- backend/functiontest.py
+++ backend/functiontest.py
@@ -1,27 +1,99 @@
-from script import *
 import unittest
+from script import *
 
-class TestMiscFunctions(unittest.TestCase):
+class TestClearBoard(unittest.TestCase):
     def testClearBoard(self):
         result = clearBoard()
         self.assertEqual(result, "Board Cleared")
+
+
+class TestIsSquareValid(unittest.TestCase):
+    def testInvalidInput(self):
+        sqr = "f"
+        result = isSquareValid(sqr)
+        self.assertEqual(result, False)
+    
+    def testInvalidFile(self):
+        sqr = ("z", 6)
+        result = isSquareValid(sqr)
+        self.assertEqual(result, False)
+    
+    def testInvalidRank(self):
+        sqr = ("a", 9)
+        result = isSquareValid(sqr)
+        self.assertEqual(result, False)
+
+    def testValidSquare(self):
+        sqr = ("g", 6)
+        result = isSquareValid(sqr)
+        self.assertEqual(result, True)
+
+
+class TestGetBoardIndices(unittest.TestCase):
+    def testGetIndicesRegular(self):
+        testCoords = ("b", 5)
+        result = getBoardIndexFromRankAndFile(testCoords)
+        self.assertEqual(result, (3, 1))      
+
+    def testGetIndicesKeyError(self):
+        testCoords = ("z", 5)
+        result = getBoardIndexFromRankAndFile(testCoords)
+        self.assertEqual(result, "Invalid")
+
+    def testGetIndicesNumError(self):
+        testCoords = ("a", 9)
+        result = getBoardIndexFromRankAndFile(testCoords)
+        self.assertEqual(result, "Invalid")
+
+    def testGetIndicesNumEdgeCase1(self):
+        testCoords = ("a", 8)
+        result = getBoardIndexFromRankAndFile(testCoords)
+        self.assertEqual(result, (0, 0))
         
+    def testGetIndicesNumEdgeCase2(self):
+        testCoords = ("a", 1)
+        result = getBoardIndexFromRankAndFile(testCoords)
+        self.assertEqual(result, (7, 0))
 
-    def testGetRankAndFile(self):
-        testIndices = (1,1)
-        row, col = testIndices
-        result = getRankAndFileFromBoardIndex(row, col)
-        self.assertEqual(result, ("b", 7))
 
-class TestChessPieces(unittest.TestCase):
-    def testPlacePiece(self):
-        testPiece = Pawn("Black", "5", ("a", 7))
-        resultString = placePiece(testPiece)
-        self.assertEqual(resultString, "BP placed")
+class TestFilterListForSquares(unittest.TestCase):
+    def testEmptyList(self):
+        lst = []
+        result = filterListForSquares(lst)
+        self.assertEqual(result, lst)
+
+    def testSingleElement(self):
+        lst = [("a", 6)]
+        result = filterListForSquares(lst)
+        self.assertEqual(result, lst)
+
+    def testSingleElementFalse(self):
+        lst = [False]
+        result = filterListForSquares(lst)
+        self.assertEqual(result, [])    
+        
+    def testTwoElement(self):
+        lst = [False, ("a", 6)]
+        result = filterListForSquares(lst)
+        self.assertEqual(result, [("a", 6)])
+
+
+class TestGetRankAndFileFromBoardIndex(unittest.TestCase):
+    def testInvalidInputs(self):
+        params = ("a", 9)
+        result = getRankAndFileFromBoardIndex(*params)
+        self.assertEqual(result, "Invalid")
+
+    def testInvalidInputs2(self):
+        params = (-2, "h")
+        result = getRankAndFileFromBoardIndex(*params)
+        self.assertEqual(result, "Invalid")
+
+    def testValidInput(self):
+        params = (4, 4)
+        result = getRankAndFileFromBoardIndex(*params)
+        self.assertEqual(result, ("e", 4))
+
+
 
 unittest.main()
-
-
-
-
-
