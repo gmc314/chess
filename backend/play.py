32b3fb2015d0fe711b295778a9cde595af8f5ef4
gmc314 <grant.cheng1@gmail.com>
2024-03-07 12:12:22 -0500

    fix colours and implement clear screen

--- backend/play.py
+++ backend/play.py
@@ -1,5 +1,9 @@
 from pprint import pprint
 from script import *
+import os
+
+def clear():
+    os.system("cls")
 
 def newGame():
     clearBoard()
@@ -39,35 +43,35 @@ def newGame():
 
     wk = King("White", "17", ('e', 1))
     placePiece(wk)
-    wq = Queen("Black", "18", ('d', 1))
+    wq = Queen("White", "18", ('d', 1))
     placePiece(wq)
-    wb1 = Bishop("Black", "19", ('c', 1))
+    wb1 = Bishop("White", "19", ('c', 1))
     placePiece(wb1)
-    wb2 = Bishop("Black", "20", ('f', 1))
+    wb2 = Bishop("White", "20", ('f', 1))
     placePiece(wb2)
-    wn1 = Knight("Black", "21", ('b', 1))
+    wn1 = Knight("White", "21", ('b', 1))
     placePiece(wn1)
-    wn2 = Knight("Black", "22", ('g', 1))
+    wn2 = Knight("White", "22", ('g', 1))
     placePiece(wn2)
-    wr1 = Rook("Black", "23", ('a', 1))
+    wr1 = Rook("White", "23", ('a', 1))
     placePiece(wr1)
-    wr2 = Rook("Black", "24", ('h', 1))
+    wr2 = Rook("White", "24", ('h', 1))
     placePiece(wr2)
-    wp1 = Pawn("Black", "25", ('a', 2))
+    wp1 = Pawn("White", "25", ('a', 2))
     placePiece(wp1)
-    wp2 = Pawn("Black", "26", ('b', 2))
+    wp2 = Pawn("White", "26", ('b', 2))
     placePiece(wp2)
-    wp3 = Pawn("Black", "27", ('c', 2))
+    wp3 = Pawn("White", "27", ('c', 2))
     placePiece(wp3)
-    wp4 = Pawn("Black", "28", ('d', 2))
+    wp4 = Pawn("White", "28", ('d', 2))
     placePiece(wp4)
-    wp5 = Pawn("Black", "29", ('e', 2))
+    wp5 = Pawn("White", "29", ('e', 2))
     placePiece(wp5)
-    wp6 = Pawn("Black", "30", ('f', 2))
+    wp6 = Pawn("White", "30", ('f', 2))
     placePiece(wp6)
-    wp7 = Pawn("Black", "31", ('g', 2))
+    wp7 = Pawn("White", "31", ('g', 2))
     placePiece(wp7)
-    wp8 = Pawn("Black", "32", ('h', 2))
+    wp8 = Pawn("White", "32", ('h', 2))
     placePiece(wp8)
     return "Board ready"
 
@@ -99,6 +103,7 @@ def extractMoveElements(player: Player, playerInput: str):
 # plan for the main playGame function
 def playGame():
     newGame()
+    pprint(BOARD)
     whiteKing = [piece for piece in WHITE.pieces if isinstance(piece, King)][0]
     blackKing = [piece for piece in BLACK.pieces if isinstance(piece, King)][0]
     numRounds = 0
@@ -116,6 +121,7 @@ def playGame():
             whiteMoveText = input("White: Enter [symbol] [currentSquare] [newSquare]: ")
             whiteMoveInput = extractMoveElements(WHITE, whiteMoveText)
             whiteMove = moveFromCurrentSquare(**whiteMoveInput)
+        clear()
         pprint(BOARD)
         
         # Black turn 
@@ -130,6 +136,7 @@ def playGame():
             blackMoveText = input("Black: Enter [symbol] [currentSquare] [newSquare]: ")
             blackMoveInput = extractMoveElements(WHITE, blackMoveText)
             blackMove = moveFromCurrentSquare(**whiteMoveInput)
+        clear()
         pprint(BOARD)
         
         numRounds += 1
@@ -141,4 +148,5 @@ def playGame():
 
     print(gameOverMessage)
     return
-        
+
+playGame()
